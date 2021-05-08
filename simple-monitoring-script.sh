a599c84b3ee534e30307e5023bf9f6513c1f1cb5
Thomas DDev <18127378+thomas-ddev@users.noreply.github.com>
2021-05-08 11:07:15 +0200

    Update simple-monitoring-script.sh

--- simple-monitoring-script.sh
+++ simple-monitoring-script.sh
@@ -16,8 +16,10 @@ DATENGINX="$(date +%Y/%m/%d)"
 echo "Vérification des erreurs OOM..."
 
 if test $(grep -i -r "out of memory" /var/log/ | grep "$DATESYS1" | grep "$DATESYS2" | wc -c) -ne 0
+     LOG=$(grep -i -r "out of memory" /var/log/ | grep "$DATESYS1" | grep "$DATESYS2")
+     
      then
-          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Un processus a atteint la limite de mémoire disponible !" --body "Un des processus du serveur a subit un OOM (Out Of Memory) et a ainsi été tué. Cela peut signifier que le serveur nécesite soit une optimisation de la gestion de sa RAM, soit un upgrade. (grep -i -r 'out of memory' /var/log)"
+          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Un processus a atteint la limite de mémoire disponible !" --body "Un des processus du serveur a subit un OOM (Out Of Memory) et a ainsi été tué. Cela peut signifier que le serveur nécesite soit une optimisation de la gestion de sa RAM, soit un upgrade. \n Pour vérifier les logs, utilisez la commande : grep -i -r 'out of memory' /var/log \n\n Erreurs : \n $LOG"
      else
           echo "Aucune erreur OOM détectée."
 fi
@@ -26,24 +28,30 @@ fi
 echo "Vérification de la présence d'erreurs PHP max_children..."
 
 if test $(cat /var/log/php7.3-fpm.log | grep "max_children" | grep "$DATEPHP" | wc -c) -ne 0
+     LOG=$(cat /var/log/php7.3-fpm.log | grep "max_children" | grep "$DATEPHP")
+
      then
-          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Variable max_children atteinte !" --body "La variable max_children de la pool PHP a été atteinte. Si les ressources le permettent, il faut songer à l'augmenter. (cat /var/log/php7.3-fpm.log | grep 'max_children' && cat /var/log/php7.3-fpm.log.1 | grep 'max_children')"
+          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Variable max_children atteinte !" --body "La variable max_children de la pool PHP a été atteinte. Si les ressources le permettent, il faut songer à l'augmenter. \n Pour vérifier les logs, utilisez la commande : cat /var/log/php7.3-fpm.log | grep 'max_children' && cat /var/log/php7.3-fpm.log.1 | grep 'max_children' \n\n Erreurs : \n $LOG"
      else
           echo "Aucune erreur PHP max_children détectée."
 fi
 
 # Vérifier s'il y a des erreurs SQL
 if test $(cat /var/log/mysql/error.log | grep "error" | grep "$DATEMYSQL" | wc -c) -ne 0
+     LOG=$(cat /var/log/mysql/error.log | grep "error" | grep "$DATEMYSQL")
+     
      then
-          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Des erreurs MySQL sont présentes dans les logs !" --body "Une ou plusieurs erreurs ont étés détectées dans les logs MySQL. Cela peut signifier que le serveur nécessite une optimisation de sa configuration MySQL. (cat /var/log/mysql/error.log | grep 'error' && zcat /var/log/mysql/error.log.1.gz | grep 'error')"
+          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Des erreurs MySQL sont présentes dans les logs !" --body "Une ou plusieurs erreurs ont étés détectées dans les logs MySQL. Cela peut signifier que le serveur nécessite une optimisation de sa configuration MySQL. \n Pour vérifier les logs, utilisez la commande : cat /var/log/mysql/error.log | grep 'error' && zcat /var/log/mysql/error.log.1.gz | grep 'error' \n\n Erreurs : \n $LOG"
      else
           echo "Aucune erreur SQL détectée."
 fi
 
 # Vérifier s'il y a des erreurs Nginx
 if test $(cat /var/log/nginx/error.log | grep "crit" | grep "$DATENGINX" | wc -c) -ne 0
+     LOG=$(cat /var/log/nginx/error.log | grep "crit" | grep "$DATENGINX")
+
      then
-          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Des erreurs Nginx sont présentes dans les logs !" --body "Une ou plusieurs erreurs ont étés détectées dans les logs Nginx. Cela peut signifier que le serveur nécessite une optimisation de sa configuration Nginx. (cat /var/log/nginx/error.log | grep 'crit' && cat /var/log/nginx/error.log.1 | grep 'crit')"
+          swaks -t $EMAIL -s $MAILSERVER -tls -au $EMAIL --ap $PASSWORD -f $EMAIL --h-Subject "[ALERTE] [$HOSTNAME] Des erreurs Nginx sont présentes dans les logs !" --body "Une ou plusieurs erreurs ont étés détectées dans les logs Nginx. Cela peut signifier que le serveur nécessite une optimisation de sa configuration Nginx. \n Pour vérifier les logs, utilisez la commande : cat /var/log/nginx/error.log | grep 'crit' && cat /var/log/nginx/error.log.1 | grep 'crit' \n\n Erreurs : \n $LOG"
      else
           echo "Aucune erreur Nginx détectée."
 fi
